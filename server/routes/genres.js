b55d6e5017b4626172c02fe0ff79538f7a68130e
tanaytadas16 <tanaysid97@gmail.com>
2022-05-04 13:52:01 -0400

    created booksData fuction

--- server/routes/genres.js
+++ server/routes/genres.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    // res.json([]);
+    if (req.argument.length > 0) {
+        throw [400, 'No arguments needed'];
+    }
     res.json([{ name: 'Tanay' }, { name: 'Sushant' }, { name: 'Harshal' }]);
-    // return [{ name: 'Tanay' }, { name: 'Sushant' }, { name: 'Harshal' }];
 });
+router.get('/:genres', async (req, res) => {});
 module.exports = router;
