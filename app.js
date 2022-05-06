1853cde76a7eebf298e16a6aa293134bdba9f3fa
tanaytadas16 <tanaysid97@gmail.com>
2022-05-06 12:18:01 -0400

    working with redis

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const static = express.static(__dirname + '/public');
+// const static = express.static(__dirname + '/public');
 const configRoutes = require('./routes');
 const cors = require('cors');
 require('dotenv').config;
 app.use(cors());
-app.use('/public', static);
+// app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('/client/build'));
+}
 configRoutes(app);
 
 app.listen(process.env.PORT || 4000, () => {
