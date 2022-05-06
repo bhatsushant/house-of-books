64bbb99cdc85406134d859cb46da64bf09faf08e
tanaytadas16 <tanaysid97@gmail.com>
2022-05-06 12:46:43 -0400

    addded build

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static('client/build'));
 }
 configRoutes(app);
 
