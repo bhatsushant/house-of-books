e2cd036520602cd5f2845b69f73889b9b3099f34
tanaytadas16 <tanaysid97@gmail.com>
2022-05-06 20:24:55 -0400

    fdsn

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-// }
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
 configRoutes(app);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
