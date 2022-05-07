d0d963e65c1bdb01a0f2856410fb391014de393b
tanaytadas16 <tanaysid97@gmail.com>
2022-05-07 19:40:47 -0400

    ,hkh

--- app.js
+++ app.js
@@ -11,16 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 configRoutes(app);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, './client/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-    });
-}
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
 app.listen(process.env.PORT || 4000, () => {
     console.log("We've now got a server!");
     console.log('Your routes will be running on http://localhost:4000');
