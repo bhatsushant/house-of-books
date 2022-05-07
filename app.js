11e39f249bc8d6051788d61d0e19f1ecf70758c6
tanaytadas16 <tanaysid97@gmail.com>
2022-05-07 19:36:23 -0400

    terrs

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')));
+configRoutes(app);
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    const path = require('path');
-    configRoutes(app);
+    app.use(express.static(path.resolve(__dirname, './client/build')));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
     });
