5c96d20a446c83a650c76adbc2ef4769a39f70a0
tanaytadas16 <tanaysid97@gmail.com>
2022-05-07 15:40:40 -0400

    hgjhg

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')));
-configRoutes(app);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
+configRoutes(app);
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("We've now got a server!");
